808f3d8fc36ee2e9bc3ce339aa530018d6823876
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-10-01 15:49:36 +0300

    add file

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -10,9 +10,7 @@ def main():
     denominator = sys.argv[2]
 
     result = safe_divide(numerator, denominator)
-    
-    if isinstance(result, float):
-        print(f"The result of the division is {result}")
+    print(result)
 
 if __name__ == "__main__":
     main()
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,8 +2,11 @@ def safe_divide(numerator, denominator):
     try:
         numerator = float(numerator)
         denominator = float(denominator)
-        return numerator / denominator
+        return f"The result of the division is {numerator / denominator}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
+    
+      
+    
